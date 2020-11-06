abc08c33941a89b6786ad4445034cdec14109a24
Idowu Joseph <idowuayanfeoluwa@gmail.com>
2020-11-06 03:58:03 +0100

    jh

--- server.js
+++ server.js
@@ -29,6 +29,11 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
+// app.use(express.static(path.join(__dirname, '../build')));
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../build'));
+// });
+
 app.use('/api/auth', authRoutes);
 app.use('/api/category', categoryRoutes);
 app.use('/api/product', productRoutes);
